1feff8edabea912b3c80975b9babc92f0f570286
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-11 18:59:17 +0530

    add aboutpage

---
+++ src/pages/About.jsx
@@ -0,0 +1,29 @@
+export const About = () => {
+  return (
+    <div id="about" className="overflow-hidden">
+      <div className="grid grid-cols-[60%_40%]  gap-8 items-start">
+        <div>
+          <p className="text-5xl font-semibold   mb-6 m-20">
+            Manufacturers of Semi-Precious Gemstones and Fashion Jewelry
+          </p>
+          <div className="mx-20 text-wrap  w-[80%] ">
+          <p className="mb-4">
+            Our gemstone manufacturing business has been delivering unparalleled quality and craftsmanship. Based in India, we take pride in offering premium semi-precious stones that cater to diverse client needs across the country.
+          </p>
+          <p className="mb-4">
+            With a foundation rooted in 30 years of industry expertise, we specialize in the art of gemstone manufacturing. For the past 6 years, our commitment to excellence has earned us a reputation as a trusted partner for clients across India. Every gemstone we create is handcrafted to meet the highest standards of quality, ensuring that each piece reflects our dedication to precision and beauty.
+          </p>
+          <p>
+            Whether you're looking for custom-cut gemstones or stones based on astrological needs, we bring your vision to life with unmatched craftsmanship.
+          </p>
+          </div>
+        </div>
+
+        <div className="bg-red-400 h-64 w-full">
+          {/* You can replace this with an image or content */}
+          adadas
+        </div>
+      </div>
+    </div>
+  );
+};
