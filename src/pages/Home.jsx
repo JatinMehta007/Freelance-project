0550385790b365454e6a3f8bb2fc035711386cd2
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-11 18:57:52 +0530

    add about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,14 @@
 import {
-  // IconBrandGithub,
   IconBrandInstagram,
   IconBrandX,
-  // IconExchange,
   IconHome,
-  // IconNewSection,
-  // IconTerminal2,
-  // IconHome,
   IconInfoCircle,
   IconPhoto,
   IconPhone,
 } from "@tabler/icons-react";
 import { FloatingDock } from "../ui/floating";
+import { Landing } from "./LandingPage";
+import { About } from "./About";
 
 export const Home = () => {
   const links = [
@@ -62,21 +59,24 @@ export const Home = () => {
   ];
 
   return (
-  <div className="bg-black min-h-screen flex flex-col justify-between">
-    {/* Header */}
-    <div className="p-4 flex justify-between bg-red-500 text-white">
+  <div id="Home" className="bg-stone-200 min-h-screen flex flex-col justify-between">
+    {/* Navbar */}
+    <div className="p-4 flex justify-between  border-b">
       <p className="text-xl font-bold tracking-wide">Gemstone Boutique</p>
-    <div className="text-white justify-between">
-      <a href="asda"> asd</a>
-      <a href="asda"> asd</a>
-      <a href="asda"> asd</a>
-      <a href="asda"> asd</a>
+    <div className=" flex  justify-around  gap-8 w-1/2 font-mono tracking-wide font-medium">
+      <a href="#Home" className="hover:text-orange-900"> Home</a>
+      <a href="#About" className="hover:text-orange-900"> About</a>
+      <a href="asda" className="hover:text-orange-900"> Gallery</a>
+      <a href="asda" className="hover:text-orange-900"> Contact us</a>
     </div>
     </div>
-
-    <div className="w-full flex justify-center py-6">
-      <FloatingDock mobileClassName="translate-y-0" items={links} />
-    </div>
+    {/* Landing page */}
+    <Landing></Landing>
+    {/* About section */}
+    <About ></About>
+<div className="fixed bottom-0 p-4 flex justify-center w-screen  shadow-md z-50">
+  <FloatingDock mobileClassName="translate-y-0" items={links} />
+</div>
   </div>
 );
 };
