90a841fd74bb76fc3ce275b83b251a2e9ce03474
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-22 13:49:20 +0530

    change font-color

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,10 +2,10 @@ import { forwardRef } from "react";
 
 export const Footer = forwardRef((props, ref) => {
   return (
-    <footer ref={ref} className="bg-gray-100 text-center py-4 text-sm text-gray-600">
+    <footer ref={ref} className="bg-black text-center py-4 text-sm text-white">
   <div>© 2025 Gemstone Boutique. All rights reserved.</div>
   <div className="mt-1 text-xs">
-    Designed & Developed by <a href="https://portfolio-jatinmehta.vercel.app" className="text-orange-800 hover:underline">Jatin Kumar Mehta</a>
+    Designed & Developed by <a href="https://portfolio-jatinmehta.vercel.app" className="text-orange-400 hover:underline">Jatin Kumar Mehta</a>
   </div>
 </footer>
   );
