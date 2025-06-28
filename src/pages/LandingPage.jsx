f258ad26f6ca8c3a874820a88123bf2c0015d8a9
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-28 17:43:17 +0530

    change font-style

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,25 +4,23 @@ import { MovingButton } from "../ui/moving_border";
 export const Landing = () => {
   return (
     <>
-      <div className="h-screen overflow-hidden    bg-gradient-to-r  from-neutral-800 via-neutral-800 to-neutral-800">
+      <div style={{ fontFamily: '"Times New Roman", Times, serif' }}   className="h-screen overflow-hidden    bg-gradient-to-r  from-neutral-800 via-neutral-800 to-neutral-800">
         {/* Overlayed Text */}
-        <div className="grid grid-cols-[60%_40%] ">
+        <div className="grid grid-cols-[40%_60%] ">
           <div className="">
-            {/* <img src="./background.jpeg" alt="" className="rotate-90 h-[1180px]  relative bottom-[150px] opacity-90" />
-        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_90%,black)]"></div> */}
-            <div className=" relative  w-[100%]  left-16 z-30 top-32 text-6xl md:text-7xl">
+            <div className=" relative  w-[100%]  left-20 z-30 top-32 text-6xl md:text-7xl">
               <p  className=" font-normal font-s text-[#CBBBB9]  ">
                 Find The Perfect{" "}
               </p>
               {/* <ColourfulText text="Gemstone Boutique" /> */}
-              <p className="  bottom-1 font-normal text-[#CBBBB9]">Gemstone </p>
-              <p className="text-lg  w-[55%] mt-5 font-normal text-[#B8B7C1] ">
+              <p className="  mt-4 font-normal text-[#CBBBB9]">Gemstone </p>
+              <p className="text-lg  w-[83%] mt-5 font-normal text-[#B8B7C1] ">
                 With 25 years of expertise in the gemstone industry, we can help
                 you find the perfect stone tailored to your needs. Trust our
                 legacy to guide your choice with precision and care. 100%
                 Natural and lab certified gemstones
               </p>
-              <div className="flex gap-10 mt-10">
+              <div className="flex gap-10 mt-12">
                 <MovingButton>
                 <button className="  text-black bg-[#EFEDE8] font-normal uppercase border  px-10 py-2  text-lg">
                   Explore
@@ -34,19 +32,22 @@ export const Landing = () => {
                 </button>
                 </MovingButton>
               </div>
-              <div className="w-[100%] p-4 grid-cols-2  grid mt-12 bg-white rounded-xl">
-                <div className="flex  justify-center items-center">
-                <img src="./diploma.png" alt="" className="w-14 h-14 "/>
-                <div className="text-xs  m-5 text-[#2B391F]">
+              <div>
+
+              </div>
+              <div className="w-[750px] absolute px-8 py-6 grid-cols-2  grid mt-12 bg-white rounded-xl">
+                <div className="flex  justify-center items-center ">
+                <img src="./diploma.png" alt="" className="w-14 h-14  "/>
+                <div className="text-xs  w-[70%] text-[#2B391F] ml-3">
                 <p className="text-sm font-bold">Certified</p>
-                <p className="font-normal">Our brand has legally certified by the authorities. No need to worry because all our product are assured.</p>
+                <p className="font-normal tracking-wider">Our brand has legally certified by the authorities. No need to worry because all our product are assured.</p>
                 </div>
                 </div>
                 <div className="flex justify-center items-center">
                 <img src="./account.png" alt="" className="w-12 h-12 "/>
-                <div className="text-xs m-5 text-[#2B391F]">
+                <div className="text-xs w-[70%] text-[#2B391F] ml-3">
                 <p className="text-sm font-bold">Authentic Sourcing</p>
-                <p className="font-normal">We procure gemstones directly from
+                <p className="font-normal tracking-wider">We procure gemstones directly from
                     mines and real sources, ensuring
                     unmatched traceability.</p>
                 </div>
@@ -59,10 +60,10 @@ export const Landing = () => {
               <img
                 src="./back1.png"
                 alt=""
-                className="rounded-full absolute w-72 top-[20%] right-[25%]"
+                className="rounded-full absolute w-72 top-[18%] right-[30%]"
               />
             </div>
-            <div className="lg:w-[138%] mt-[25%] overflow-hidden">
+            <div className="lg:w-[100%] ml-40   mt-[15%]  overflow-hidden">
               <img src="./backs2.jpg" alt="" className="rounded-full w-full" />
             </div>
           </div>
