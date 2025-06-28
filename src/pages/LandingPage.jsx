12b3cd1c222fffbcfa2544e4174336d3a1e91c6a
Jatin_Mehta <mehtajatin045@gmail.com>
2025-06-28 15:55:31 +0530

    change color

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,19 +4,19 @@ import { MovingButton } from "../ui/moving_border";
 export const Landing = () => {
   return (
     <>
-      <div className="h-screen overflow-hidden  font-black bg-gradient-to-r  from-neutral-800 via-neutral-800 to-neutral-800">
+      <div className="h-screen overflow-hidden   font-black bg-gradient-to-r  from-neutral-800 via-neutral-800 to-neutral-800">
         {/* Overlayed Text */}
         <div className="grid grid-cols-[60%_40%] ">
           <div className="">
             {/* <img src="./background.jpeg" alt="" className="rotate-90 h-[1180px]  relative bottom-[150px] opacity-90" />
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_90%,black)]"></div> */}
-            <div className=" relative  w-[100%]  left-15 z-30 top-32 text-6xl md:text-7xl">
-              <p className=" font-light  text-indigo-100  ">
+            <div className=" relative  w-[100%]  left-16 z-30 top-32 text-6xl md:text-7xl">
+              <p className=" font-light  text-[#CBBBB9]  ">
                 Find The Perfect{" "}
               </p>
               {/* <ColourfulText text="Gemstone Boutique" /> */}
-              <p className="  bottom-1 font-light text-indigo-100">Gemstone </p>
-              <p className="text-xl  w-[55%] mt-5  text-white font-light">
+              <p className="  bottom-1 font-light text-[#CBBBB9]">Gemstone </p>
+              <p className="text-xl  w-[55%] mt-5  text-[#B8B7C1] font-light">
                 With 25 years of expertise in the gemstone industry, we can help
                 you find the perfect stone tailored to your needs. Trust our
                 legacy to guide your choice with precision and care. 100%
@@ -24,7 +24,7 @@ export const Landing = () => {
               </p>
               <div className="flex gap-10 mt-10">
                 <MovingButton>
-                <button className=" rounded-lg text-black bg-white font-light uppercase border  px-10 py-2  text-lg">
+                <button className=" rounded-lg text-black bg-[#EFEDE8] font-light uppercase border  px-10 py-2  text-lg">
                   Explore
                 </button>
                 </MovingButton>
@@ -34,7 +34,7 @@ export const Landing = () => {
                 </button>
                 </MovingButton>
               </div>
-              <div className="w-[700px] p-2  grid-cols-2  grid mt-12 bg-white rounded-xl">
+              <div className="w-[700px] p-2   grid-cols-2  grid mt-12 bg-white rounded-xl">
                 <div className="flex  justify-center items-center">
                 <img src="./diploma.png" alt="" className="w-14 h-14 "/>
                 <div className="text-xs  m-5 font-normal">
@@ -62,7 +62,7 @@ export const Landing = () => {
                 className="rounded-full absolute w-72 top-[20%] right-[25%]"
               />
             </div>
-            <div className="w-[800px] mt-[25%] overflow-hidden">
+            <div className="lg:w-[138%] mt-[25%] overflow-hidden">
               <img src="./backs2.jpg" alt="" className="rounded-full w-full" />
             </div>
           </div>
