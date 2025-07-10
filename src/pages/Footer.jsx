b8fa21b0d603f792dc4e4d4d99cec76df124ed1b
Jatin_Mehta <mehtajatin045@gmail.com>
2025-07-11 00:22:08 +0530

    Add quote

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,11 +2,24 @@ import { forwardRef } from "react";
 
 export const Footer = forwardRef((props, ref) => {
   return (
-    <footer ref={ref} className="bg-gradient-to-r from-neutral-800 via-neutral-800 to-neutral-800 text-center py-4 text-sm text-white">
-  <div>© 2025 Gemstone Boutique. All rights reserved.</div>
-  <div className="mt-1 text-xs">
-    Designed & Developed by <a href="https://portfolio-jatinmehta.vercel.app" className="text-orange-400 hover:underline">Jatin Kumar Mehta</a>
-  </div>
+    <footer ref={ref} className="bg-neutral-900 text-white py-6 text-center  border-t border-neutral-700">
+  <p className="text-sm italic tracking-wide text-gray-400">
+    ~ Precision in every facet ~
+  </p>
+  <p className="text-sm  tracking-wide leading-6">
+    © 2025 <span className="font-semibold">Gemstone Boutique</span>. All rights reserved.
+  </p>
+  <p className="text-sm">
+    Designed & Developed by{" "}
+    <a
+      href="https://your-portfolio-link.com"
+      className="font-semibold text-orange-400 hover:text-orange-300 transition duration-300"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Jatin Kumar Mehta
+    </a>
+  </p>
 </footer>
   );
 });
